cdb89cf1573b3fdb9841580dd474fe5a90778097
eduardonv <eduardo.n.vicentini@gmail.com>
2023-01-17 17:35:47 -0300

    ec2-6

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
-
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+  }
+  
+  backend "s3" {
+    bucket = "terraform-state-eduardo"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
   }
 }
 
