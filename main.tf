b8bcd9aa95176db6354ca592d55a2eff7f9b1d84
eduardonv <eduardo.n.vicentini@gmail.com>
2023-01-17 12:27:35 -0300

    ec2-v2

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "k8s" {
     egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
+    protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
